dee9513ae4abbe205685f1d1542d6b0ddc13940c
GiannfrancoCrovetto <giannfranco.crovetto@arcadis.com>
2026-07-18 22:08:21 -0400

    did bug fixes and added user repeatability

--- calculator.sh
+++ calculator.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 echo "Hello, Bash!"
 #! Calculator
-echo "Enter first number:"
-read a
+while true; do
+    echo "Enter first number:"
+    read a
 
-echo "Enter second number:"
-read b
+    echo "Enter second number:"
+    read b
 
-echo "Enter choice:"
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Division"
-read ch
+    echo "Enter choice:"
+    echo "1. Addition"
+    echo "2. Subtraction"
+    echo "3. Multiplication"
+    echo "4. Division"
+    read ch
 
-case $ch in
-    1) res=$((a + b)) ;;
-    2) res=$((a - b)) ;;
-    3) res=$((a * b)) ;;
-    4) res=$((a / b)) ;;
-esac
+    case $ch in
+        1) res=$((a + b)) ;;
+        2) res=$((a - b)) ;;
+        3) res=$((a * b)) ;;
+        4) res=$((a / b)) ;;
+    esac
 
-echo "Result: $res"
+    echo "Result: $res"
+
+    echo "Go again? (y/n)"
+    read again
+    if [[ "$again" != "y" && "$again" != "Y" ]]; then
+        break
+    fi
+done
