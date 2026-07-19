ca2a7ec110c68028811ad110c9504e363595237a
GiannfrancoCrovetto <giannfranco.crovetto@arcadis.com>
2026-07-18 21:51:53 -0400

    added 2 shell files

---
+++ calculator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello, Bash!"
+#! Calculator
+echo "Enter first number:"
+read a
+
+echo "Enter second number:"
+read b
+
+echo "This is a test"; echo "This is another test"
+
+name="World"
+echo "Hello, $name!"
---
+++ syntax.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hello, Bash!"
+
+echo "This is a test"; echo "This is another test"
+
+name="World"
+echo "Hello, $name!"
