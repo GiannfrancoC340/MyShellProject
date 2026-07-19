315088c2ed8bf0dd91c6e6b0ecef14f6ea1fd3e4
GiannfrancoCrovetto <giannfranco.crovetto@arcadis.com>
2026-07-18 22:01:26 -0400

    added the calculator

--- calculator.sh
+++ calculator.sh
@@ -6,3 +6,19 @@ read a
 
 echo "Enter second number:"
 read b
+
+echo "Enter choice:"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch
+
+case $ch in
+    1) res=$((a + b)) ;;
+    2) res=$((a - b)) ;;
+    3) res=$((a * b)) ;;
+    4) res=$((a / b)) ;;
+esac
+
+echo "Result: $res"
